fcbdd228558493c6884b5f1698e6df8d266d1807
midhila123 <midhilacv@gmail.com>
2024-12-11 11:31:34 +0530

    initial commit

---
+++ program1.js
@@ -0,0 +1,9 @@
+function addnumbers(num1,num2){
+
+return num1+num2;
+
+}
+const number1=2;
+const number2=7;
+const result=addnumbers(number1,number2);
+console.log('the number1 and number2 is result');
